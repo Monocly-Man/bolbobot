4788dc5d35990c1d3626981b7295142f10f472ef
Monocly-Man <88500951+Monocly-Man@users.noreply.github.com>
2021-08-05 23:24:41 +0800

    Add files via upload

---
+++ alias.py
@@ -0,0 +1,810 @@
+MOVE_NAMES = [
+    {
+        "name": "fast elbow",
+        "alias": ["fast elbow"]
+    },
+    {
+        "name": "knife hand strike",
+        "alias": ["knife hand strike", "knife hand", "knife", "khs"]
+    },
+    {
+        "name": "hook",
+        "alias": ["hook"]
+    },
+    {
+        "name": "jab punch",
+        "alias": ["jab punch", "jab"]
+    },
+    {
+        "name": "fast back fist",
+        "alias": ["fast back fist", "fbfbad", "fbadf"]
+    },
+    {
+        "name": "spin elbow",
+        "alias": ["spin elbow"]
+    },
+    {
+        "name": "parry & strike",
+        "alias": ["parry & strike", "pns", "p&s", "parry and strike"]
+    },
+    {
+        "name": "straight punch",
+        "alias": ["straight punch"]
+    },
+    {
+        "name": "low spin heel",
+        "alias": ["low spin heel", "crutch spin heel", "lsh", "spin heel"]
+    },
+    {
+        "name": "roll punch",
+        "alias": ["roll punch"]
+    },
+    {
+        "name": "axe kick",
+        "alias": ["axe kick", "neutch", "axe"]
+    },
+    {
+        "name": "low kick",
+        "alias": ["low kick"]
+    },
+    {
+        "name": "side kick",
+        "alias": ["side kick", "sk"]
+    },
+    {
+        "name": "back fist",
+        "alias": ["back fist", "backfist", "other cleaver", "cleaver 2"]
+    },
+    {
+        "name": "spin back fist",
+        "alias": ["spin back fist", "sbf", "worse bura"]
+    },
+    {
+        "name": "cross punch",
+        "alias": ["cross punch"]
+    },
+    {
+        "name": "pushed elbow",
+        "alias": ["pushed elbow", "push elbow"]
+    },
+    {
+        "name": "roll back fist",
+        "alias": ["roll back fist", "rollback fist", "roll backfist", "rbf", "rollback", "roll back"]
+    },
+    {
+        "name": "spinning high kick",
+        "alias": ["spinning high kick", "shk"]
+    },
+    {
+        "name": "pushed back kick",
+        "alias": ["pushed back kick", "pbk", "push back kick"]
+    },
+    {
+        "name": "roll uppercut",
+        "alias": ["roll uppercut"]
+    },
+    {
+        "name": "front kick",
+        "alias": ["front kick", "front"]
+    },
+    {
+        "name": "charged haymaker",
+        "alias": ["charged haymaker", "haymaker"]
+    },
+    {
+        "name": "jumped spin kick",
+        "alias": ["jumped spin kick", "jsk", "best move", "coolest move", "sexiest move"]
+    },
+    {
+        "name": "hammer kick",
+        "alias": ["hammer kick", "hammer", "worse front kick", "cooler front kick"]
+    },
+    {
+        "name": "jumped light kick",
+        "alias": ["jumped light kick", "jlk"]
+    },
+    {
+        "name": "winged back kick",
+        "alias": ["winged back kick", "wbk", "winged", "wing back kick"]
+    },
+    {
+        "name": "fast punch",
+        "alias": ["fast punch", "accurate name"]
+    },
+    {
+        "name": "knee strike",
+        "alias": ["knee strike"]
+    },
+    {
+        "name": "mawashi",
+        "alias": ["mawashi"]
+    },
+    {
+        "name": "uramawashi",
+        "alias": ["uramawashi", "ura"]
+    },
+    {
+        "name": "360 tornado kick",
+        "alias": ["360 tornado kick", "360 kick", "360", "tornado", "tornado kick"]
+    },
+    {
+        "name": "illusion twist kick",
+        "alias": ["illusion twist kick", "itk", "illusion"]
+    },
+    {
+        "name": "falcon punch",
+        "alias": ["falcon punch", "falcon", "bad move"]
+    },
+    {
+        "name": "upper elbow",
+        "alias": ["upper elbow", "big yeet"]
+    },
+    {
+        "name": "spinning wide hook",
+        "alias": ["spinning wide hook", "swh"]
+    },
+    {
+        "name": "furious uppercut",
+        "alias": ["furious uppercut"]
+    },
+    {
+        "name": "vertical slash",
+        "alias": ["vertical slash"]
+    },
+    {
+        "name": "head splitter",
+        "alias": ["painstaking slash"]
+    },
+    {
+        "name": "light thrust",
+        "alias": ["light thrust"]
+    },
+    {
+        "name": "parry slash",
+        "alias": ["parry slash"]
+    },
+    {
+        "name": "rising slash",
+        "alias": ["rising slash"]
+    },
+    {
+        "name": "ram thrust",
+        "alias": ["ram thrust", "ram"]
+    },
+    {
+        "name": "one handed slash",
+        "alias": ["one handed slash", "one-handed slash", "one hand slash" "ohs"]
+    },
+    {
+        "name": "keen crouch",
+        "alias": ["keen crouch", "keen"]
+    },
+    {
+        "name": "side wind thrust",
+        "alias": ["side wind thrust", "sidewind thrust", "side wind", "sidewind"]
+    },
+    {
+        "name": "ducking spike",
+        "alias": ["ducking spike"]
+    },
+    {
+        "name": "dash slash",
+        "alias": ["dash slash"]
+    },
+    {
+        "name": "seven star thrust",
+        "alias": ["seven star thrust", "seven star"]
+    },
+    {
+        "name": "pommel bash",
+        "alias": ["pommel bash"]
+    },
+    {
+        "name": "circular slash",
+        "alias": ["circular slash", "circular"]
+    },
+    {
+        "name": "light swing slash",
+        "alias": ["light swing slash", "lss", "light swing"]
+    },
+    {
+        "name": "rising spin slash",
+        "alias": ["rising spin slash"]
+    },
+    {
+        "name": "mill slash",
+        "alias": ["mill slash"]
+    },
+    {
+        "name": "limbo thrust",
+        "alias": ["limbo thrust", "limbo"]
+    },
+    {
+        "name": "thunder slash",
+        "alias": ["thunder slash", "thunder"]
+    },
+    {
+        "name": "ground swell slash",
+        "alias": ["ground swell slash", "gss", "ground swell"]
+    },
+    {
+        "name": "reverse rising slash",
+        "alias": ["reverse rising slash", "rrs"]
+    },
+    {
+        "name": "meia lua",
+        "alias": ["meia lua", "meialua", "meia", "meia-lua", "worse side kick"]
+    },
+    {
+        "name": "reverse sharp slash",
+        "alias": ["reverse sharp slash"]
+    },
+    {
+        "name": "back thrust",
+        "alias": ["back thrust"]
+    },
+    {
+        "name": "body slicing",
+        "alias": ["body slicing"]
+    },
+    {
+        "name": "reverse twist slash",
+        "alias": ["reverse twist slash"]
+    },
+    {
+        "name": "jackhammer punch",
+        "alias": ["jackhammer punch", "jackhammer"]
+    },
+    {
+        "name": "reverse hips slash",
+        "alias": ["reverse hips slash", "reverse hips"]
+    },
+    {
+        "name": "curled up uppercut",
+        "alias": ["curled up uppercut", "cuu", "curled up upper", "curled up"]
+    },
+    {
+        "name": "body blow",
+        "alias": ["body blow"]
+    },
+    {
+        "name": "reverse one handed slash",
+        "alias": ["reverse one handed slash", "reverse one handed"]
+    },
+    {
+        "name": "parry reverse low slash",
+        "alias": ["parry reverse low slash"]
+    },
+    {
+        "name": "cleaver blow",
+        "alias": ["cleaver blow", "cleaver", "cleaber"]
+    },
+    {
+        "name": "side thrust",
+        "alias": ["side thrust"]
+    },
+    {
+        "name": "leg breaker",
+        "alias": ["leg breaker", "roller skatey ankle breaky", "roller skatey ankle breakey", "rsab"]
+    },
+    {
+        "name": "rising double hand",
+        "alias": ["rising double hand", "fastest gb"]
+    },
+    {
+        "name": "surging palm",
+        "alias": ["surging palm", "surging"]
+    },
+    {
+        "name": "whirl slash",
+        "alias": ["whirl slash"]
+    },
+    {
+        "name": "reverse feet thrust",
+        "alias": ["reverse feet thrust"]
+    },
+    {
+        "name": "reverse rising thrust",
+        "alias": ["reverse rising thrust"]
+    },
+    {
+        "name": "twist hips slash",
+        "alias": ["twist hips slash", "twist hips"]
+    },
+    {
+        "name": "tetsuzanko",
+        "alias": ["tetsuzanko", "tetsu"]
+    },
+    {
+        "name": "wallop blow",
+        "alias": ["wallop blow", "wallop"]
+    },
+    {
+        "name": "double palm",
+        "alias": ["double palm"]
+    },
+    {
+        "name": "slap kick",
+        "alias": ["slap kick"]
+    },
+    {
+        "name": "spiral slash",
+        "alias": ["spiral slash"]
+    },
+    {
+        "name": "soto-uke",
+        "alias": ["soto-uke", "soto uke", "soto"]
+    },
+    {
+        "name": "pulmonary palm",
+        "alias": ["pulmonary palm", "pulmonary"]
+    },
+    {
+        "name": "digging parry elbow",
+        "alias": ["digging parry elbow"]
+    },
+    {
+        "name": "parry pommel bash",
+        "alias": ["parry pommel bash"]
+    },
+    {
+        "name": "swirl slash",
+        "alias": ["swirl slash", "swirl"]
+    },
+    {
+        "name": "kitsuneo cut",
+        "alias": ["kitsuneo cut", "kitsuneo", "kitsune cut", "kitsune"]
+    },
+    {
+        "name": "ankle stamp",
+        "alias": ["ankle stamp", "ankle"]
+    },
+    {
+        "name": "dwit chagi",
+        "alias": ["dwit chagi", "dwit"]
+    },
+    {
+        "name": "crushing palm",
+        "alias": ["crushing palm", "crushing"]
+    },
+    {
+        "name": "parry shove",
+        "alias": ["parry shove"]
+    },
+    {
+        "name": "front sweep",
+        "alias": ["front sweep"]
+    },
+    {
+        "name": "puropera cut",
+        "alias": ["puropera cut", "puropera"]
+    },
+    {
+        "name": "duster blow",
+        "alias": ["duster blow", "duster"]
+    },
+    {
+        "name": "collar chop",
+        "alias": ["collar chop", "collar"]
+    },
+    {
+        "name": "shapu furiko",
+        "alias": ["shapu furiko", "shapu", "furiko"]
+    },
+    {
+        "name": "liver knee",
+        "alias": ["liver knee", "liver"]
+    },
+    {
+        "name": "tei-nami",
+        "alias": ["tei-nami", "teinami", "tei nami"]
+    },
+    {
+        "name": "needle point",
+        "alias": ["needle point", "needle"]
+    },
+    {
+        "name": "buchinmo",
+        "alias": ["buchinmo", "brrchinmo"]
+    },
+    {
+        "name": "bending palm",
+        "alias": ["bending palm", "bending"]
+    },
+    {
+        "name": "underknee kick",
+        "alias": ["underknee kick", "underknee", "under knee kick", "under knee"]
+    },
+    {
+        "name": "back tripped kick",
+        "alias": ["back tripped kick", "btk"]
+    },
+    {
+        "name": "crouching elbow",
+        "alias": ["crouching elbow", "crouch elbow"]
+    },
+    {
+        "name": "bounce knee",
+        "alias": ["bounce knee", "bounce"]
+    },
+    {
+        "name": "scissor kick",
+        "alias": ["scissor kick", "scissor"]
+    },
+    {
+        "name": "stretch out hook",
+        "alias": ["stretch out hook", "soh", "bad out hook"]
+    },
+    {
+        "name": "direct punch",
+        "alias": ["direct punch"]
+    },
+    {
+        "name": "chin palm",
+        "alias": ["chin palm"]
+    },
+    {
+        "name": "jump out elbow",
+        "alias": ["jump out elbow", "joe", "worse plexus"]
+    },
+    {
+        "name": "rising kick",
+        "alias": ["rising kick"]
+    },
+    {
+        "name": "twist parry strike",
+        "alias": ["twist parry strike"]
+    },
+    {
+        "name": "calbot",
+        "alias": ["calbot", "slap", "el slappo", "assert dominance"]
+    },
+    {
+        "name": "spiral palm",
+        "alias": ["spiral palm"]
+    },
+    {
+        "name": "mill punch",
+        "alias": ["mill punch"]
+    },
+    {
+        "name": "tripped kick",
+        "alias": ["tripped kick"]
+    },
+    {
+        "name": "poke thrust",
+        "alias": ["poke thrust"]
+    },
+    {
+        "name": "elbow stumble",
+        "alias": ["elbow stumble"]
+    },
+    {
+        "name": "drunk crane",
+        "alias": ["drunk crane", "crane", "op move"]
+    },
+    {
+        "name": "wobble low kick",
+        "alias": ["wobble low kick", "wobble"]
+    },
+    {
+        "name": "back turn wrist",
+        "alias": ["back turn wrist"]
+    },
+    {
+        "name": "wrist jab",
+        "alias": ["wrist jab"]
+    },
+    {
+        "name": "double spike kick",
+        "alias": ["double spike kick"]
+    },
+    {
+        "name": "whirlwind double punch",
+        "alias": ["whirlwind double punch", "wwdp", "whirlwind punch", "whirlwind"]
+    },
+    {
+        "name": "double fist stretch",
+        "alias": ["double fist stretch"]
+    },
+    {
+        "name": "backfall strike",
+        "alias": ["backfall strike", "backfall"]
+    },
+    {
+        "name": "twist back kick",
+        "alias": ["twist back kick", "twist back", "tbk"]
+    },
+    {
+        "name": "grab punch",
+        "alias": ["grab punch", "grab lunch", "lunch", "naynay's waifu"]
+    },
+    {
+        "name": "spiral back punch",
+        "alias": ["spiral back punch"]
+    },
+    {
+        "name": "stumble slash",
+        "alias": ["stumble slash"]
+    },
+    {
+        "name": "nose stab",
+        "alias": ["nose stab"]
+    },
+    {
+        "name": "foot slap",
+        "alias": ["foot slap"]
+    },
+    {
+        "name": "spoon slash",
+        "alias": ["spoon slash", "spoon"]
+    },
+    {
+        "name": "inward slash",
+        "alias": ["inward slash"]
+    },
+    {
+        "name": "eye poke",
+        "alias": ["eye poke"]
+    },
+    {
+        "name": "back hop wrist",
+        "alias": ["back hop wrist", "back hop"]
+    },
+    {
+        "name": "donkey slap",
+        "alias": ["donkey slap", "donkey"]
+    },
+    {
+        "name": "drunken paw",
+        "alias": ["drunken paw", "drunk paw", "paw"]
+    },
+    {
+        "name": "wrist roll slash",
+        "alias": ["wrist roll slash", "wrist roll"]
+    },
+    {
+        "name": "sickle slash",
+        "alias": ["sickle slash", "sickle"]
+    },
+    {
+        "name": "woosh slash",
+        "alias": ["woosh slash", "woosh'"]
+    },
+    {
+        "name": "slip slash",
+        "alias": ["slip slash"]
+    },
+    {
+        "name": "gut punch",
+        "alias": ["gut punch"]
+    },
+    {
+        "name": "drunk stomp",
+        "alias": ["drunk stomp", "drunken stomp"]
+    },
+    {
+        "name": "jar bash",
+        "alias": ["jar bash"]
+    },
+    {
+        "name": "drunken smash",
+        "alias": ["drunken smash", "drunk smash"]
+    },
+    {
+        "name": "hadrunken",
+        "alias": ["hadrunken", "teachin bitches how to swim"]
+    },
+    {
+        "name": "drop slash",
+        "alias": ["drop slash"]
+    },
+    {
+        "name": "wide slash",
+        "alias": ["wide slash"]
+    },
+    {
+        "name": "handstand kick",
+        "alias": ["handstand kick", "handstand"]
+    },
+    {
+        "name": "spinning flute swing",
+        "alias": ["spinning flute swing", "flute", "flute swing", "floot"]
+    },
+    {
+        "name": "obvious slash",
+        "alias": ["obvious slash", "obvious"]
+    },
+    {
+        "name": "typhoon slash",
+        "alias": ["typhoon slash", "typhoon"]
+    },
+    {
+        "name": "forward lean slash",
+        "alias": ["forward lean slash", "forwards lean slash"]
+    },
+    {
+        "name": "tendon slash",
+        "alias": ["tendon slash", "tendon"]
+    },
+    {
+        "name": "low backfist",
+        "alias": ["low backfist", "low back fist", "lbf", "baby wallop"]
+    },
+    {
+        "name": "uraken",
+        "alias": ["uraken"]
+    },
+    {
+        "name": "heel to knee",
+        "alias": ["heel to knee", "htk", "h2k"]
+    },
+    {
+        "name": "inside kick",
+        "alias": ["inside kick", "inside"]
+    },
+    {
+        "name": "outward kick",
+        "alias": ["outward kick", "outwards kick", "outward", "outwards"]
+    },
+    {
+        "name": "back ura",
+        "alias": ["back ura", "bura", "back back"]
+    },
+    {
+        "name": "fast cross",
+        "alias": ["fast cross"]
+    },
+    {
+        "name": "temple knock",
+        "alias": ["temple knock", "temple"]
+    },
+    {
+        "name": "blink punch",
+        "alias": ["blink punch", "blink"]
+    },
+    {
+        "name": "rabbit punch",
+        "alias": ["rabbit punch", "rabbit", "worse crane"]
+    },
+    {
+        "name": "gokai slash",
+        "alias": ["gokai slash", "gokai"]
+    },
+    {
+        "name": "light sidekick",
+        "alias": ["light sidekick", "light side kick", "lsk"]
+    },
+    {
+        "name": "shifting thrust",
+        "alias": ["shifting thrust"]
+    },
+    {
+        "name": "front stab",
+        "alias": ["front stab"]
+    },
+    {
+        "name": "fencing punch",
+        "alias": ["fencing punch", "fencing"]
+    },
+    {
+        "name": "upper backfist",
+        "alias": ["upper backfist", "upper back fist", "ubf"]
+    },
+    {
+        "name": "reaching maegeri",
+        "alias": ["reaching maegeri"]
+    },
+    {
+        "name": "jumping risekick",
+        "alias": ["jumping risekick", "jumping rise kick", "ugliest move"]
+    },
+    {
+        "name": "double wata",
+        "alias": ["double wata", "wata"]
+    },
+    {
+        "name": "switch kick",
+        "alias": ["switch kick", "switch"]
+    },
+    {
+        "name": "reaching mawashi",
+        "alias": ["reaching mawashi"]
+    },
+    {
+        "name": "plexus elbow",
+        "alias": ["plexus elbow", "plexus", "better joe"]
+    },
+    {
+        "name": "face backfist",
+        "alias": ["face backfist", "fbf"]
+    },
+    {
+        "name": "guts punch",
+        "alias": ["guts punch", "guts", "the french have terrible naming convention", "mongoman"]
+    },
+    {
+        "name": "power mawashi",
+        "alias": ["power mawashi"]
+    },
+    {
+        "name": "run-up strike",
+        "alias": ["run-up strike", "run up strike", "run-up", "run up"]
+    },
+    {
+        "name": "horse kick",
+        "alias": ["horse kick", "horse"]
+    },
+    {
+        "name": "one inch punch",
+        "alias": ["one inch punch", "one inch", "oip"]
+    },
+    {
+        "name": "double thrust",
+        "alias": ["double thrust"]
+    },
+    {
+        "name": "up slash",
+        "alias": ["up slash"]
+    },
+    {
+        "name": "hook slash",
+        "alias": ["hook slash"]
+    },
+    {
+        "name": "leg stroke",
+        "alias": ["leg stroke"]
+    },
+    {
+        "name": "thigh slash",
+        "alias": ["thigh slash", "thigh"]
+    },
+    {
+        "name": "toreador slash",
+        "alias": ["toreador slash", "toreador", "bs move"]
+    },
+    {
+        "name": "corkscrew thrust",
+        "alias": ["corkscrew thrust", "corkscrew"]
+    },
+    {
+        "name": "neck slash",
+        "alias": ["neck slash"]
+    },
+    {
+        "name": "arc slash",
+        "alias": ["arc slash"]
+    },
+    {
+        "name": "chest stab",
+        "alias": ["chest stab"]
+    },
+    {
+        "name": "overhead slash",
+        "alias": ["overhead slash"]
+    },
+    {
+        "name": "gatotsu",
+        "alias": ["gatotsu", "the poke"]
+    },
+    {
+        "name": "faejin stopper",
+        "alias": ["faejin stopper", "fstopper", "fae stopper", "front faejin", "front fae"]
+    },
+    {
+        "name": "faejin parry",
+        "alias": ["faejin parry", "fae parry", "fparry", "f parry", "faeparry"]
+    },
+    {
+        "name": "back stagger",
+        "alias": ["back stagger", "backstagger", "bstagger"]
+    },
+    {
+        "name": "front stagger",
+        "alias": ["front stagger", "frontstagger", "fstagger"]
+    },
+    {
+        "name": "side stagger",
+        "alias": ["side stagger", "sidestagger", "sstagger"]
+    },
+]
---
+++ bolbobot.py
@@ -0,0 +1,109 @@
+# Absolver Frame Data bot (bolbobot), by Monocly Man
+# Created 25th of July 2021 in python version 3.9.6
+# Last edited 5th August 2021
+import os
+import json
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+import alias
+
+# Variables
+__version__ = str("1.1")
+dirname = os.path.dirname(__file__)
+imglink = str("https://absolver.dev/assets/images/")
+
+
+# Functions
+def get_move(move_name):
+    filepath = dirname + "/movelist.json"
+    with open(filepath) as movelist_file:
+        contents = movelist_file.read()
+    movelist_json = json.loads(contents)
+
+    move_details = list(filter(lambda x: (x['Attack'].lower() == move_name), movelist_json))
+
+    return move_details[0]
+
+
+def move_embed(move):
+    imgname = move['Attack'].replace(" ", "-")
+    imgname = imgname.lower()
+
+    embed = discord.Embed(title=move['Attack'], colour=0x1f3c80)
+    if imgname == "faejin-stopper" or imgname == "faejin-parry":
+        embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/c/ca/Bruce_Lee_1973.jpg")
+        print("https://upload.wikimedia.org/wikipedia/commons/c/ca/Bruce_Lee_1973.jpg")
+    elif imgname == "back-stagger" or imgname == "front-stagger" or imgname == "side-stagger":
+        embed.set_thumbnail(
+            url="https://www.michaelshouse.com/wp-content/uploads/2010/10/alcoholic-man-silhouette-600.jpg"
+        )
+        print("https://www.michaelshouse.com/wp-content/uploads/2010/10/alcoholic-man-silhouette-600.jpg")
+    else:
+        embed.set_thumbnail(url=imglink + imgname + ".png")
+
+    # Shut the fuck up I know it's ugly i cbf to make it nicer
+    embed.add_field(name='Start Up \u200B', value=move['Start up frame'],)
+    embed.add_field(name='Hit \u200B', value=move['Adv. on hit'])
+    embed.add_field(name='Block \u200B', value=move['Adv. on block'])
+    embed.add_field(name='Height', value=move['Target Area'])
+    embed.add_field(name='Type', value=move['Attack Type'])
+    embed.add_field(name='Parry Direction', value=move['Target Side'])
+    embed.add_field(name='Impact', value=move['Impact'])
+    embed.add_field(name='Special', value=move['Special properties'])
+    embed.add_field(name='Transition', value=move['Stance Transition'])
+    embed.add_field(name='Notes', value=move['Notes'])
+
+    return embed
+
+
+# Main
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!bolbo ')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord! Running version {__version__}\n'
+          f'----------------------------------------'
+          )
+
+
+@bot.command(name="get", help="Acquires the frame data of the named move. Aliases accepted, swords not yet available.")
+async def getmove(ctx):
+    user_message = ctx.message.content
+    user_message = user_message.replace("!bolbo get ", "")
+
+    move_name = user_message.lower()
+
+    # Searches alias.py for aliases
+    move_alias = list(filter(lambda x: (move_name in x["alias"]), alias.MOVE_NAMES))
+    if move_alias:
+        move_name = move_alias[0]["name"]
+        # I know using try except is lazy and bad but i am lazy and bad
+        try:
+            move = get_move(move_name)
+            embed = move_embed(move)
+            response = embed
+        except:
+            response = discord.Embed(title="Data not found", colour=0x1f3c80)
+
+    else:
+        response = discord.Embed(title="Could not find specified attack", colour=0x1f3c80)
+
+    await ctx.send(embed=response)
+
+
+@bot.command(name="sneak", help="I wonder what this does...")
+async def sneak(ctx):
+    sneaklink = str("https://cdn.discordapp.com/attachments/350772748013928458/849339089668276244/deckfinder.png")
+
+    response = sneaklink
+    await ctx.send(response)
+
+
+# Runs bot
+bot.run(TOKEN)
